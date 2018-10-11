c106a2ae5176fce2bca6be60d90ea424a78ef4c0
Lucas Calje <lucas@calje.eu>
2018-10-11 17:42:13 +0200

    Added close/delete to merge unit

--- src/app/units/merge-streams/merge-streams.component.ts
+++ src/app/units/merge-streams/merge-streams.component.ts
@@ -1,9 +1,9 @@
 import { Component, Host, OnInit } from '@angular/core';
 import { MergeStreamsWorker } from '../../workers/merge-streams';
 import { FormBuilder } from '@angular/forms';
-import { XxlFlowBasedService } from '../../../../projects/flow-based/src/lib/flow-based.service';
 import { XxlFlowUnitService } from '../../../../projects/flow-based/src/lib/services/flow-unit-service';
 import { XxlFlowUnitState, XxlSocket } from '../../../../projects/flow-based/src/lib/flow-based';
+import { XxlFlowBasedService } from '../../../../projects/flow-based/src/lib/flow-based.service';
 
 @Component({
   selector: 'fb-merge-streams',
@@ -13,19 +13,32 @@ import { XxlFlowUnitState, XxlSocket } from '../../../../projects/flow-based/src
 export class MergeStreamsComponent implements OnInit {
   state: XxlFlowUnitState;
   worker: MergeStreamsWorker;
+  isActive = false;
 
   constructor(private fb: FormBuilder,
               private flowService: XxlFlowBasedService,
               @Host() private service: XxlFlowUnitService) {
     this.state = service.state;
   }
-
   ngOnInit() {
     this.worker = this.flowService.getWorker(this.state.id) as MergeStreamsWorker;
   }
 
   getSockets(): XxlSocket[] {
     return this.worker.getSockets();
+  }
+
+
+  setActive(state: boolean): void {
+    this.isActive = state;
+  }
+
+  onDelete(): void {
+    this.service.delete();
+  }
+
+  onClose(): void {
+    this.service.close();
   }
 
   get title(): string {
